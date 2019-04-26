6f086de8bd7327308abb528223b498d6e2960deb
skfwe <wcq-062821@163.com>
2019-04-26 20:27:34 +0800

    add cover function

--- html2Book.py
+++ html2Book.py
@@ -2,13 +2,16 @@
 # @Author: wcq
 # @Date:   2019-04-24 19:30:13
 # @Last Modified by:   skfwe
-# @Last Modified time: 2019-04-26 11:53:22
+# @Last Modified time: 2019-04-26 20:25:14
 import os
 import sys
 import re
 from time import sleep
 import datetime
 import json
+import random
+import hashlib
+from PIL import Image, ImageDraw, ImageFont
 
 def Usage():
     print('python3 %s <bookName> <mobi | pdfk | pdfpc | all> [SUMMARY.md | skip | ss]'%(os.sys.argv[0]))
@@ -16,6 +19,143 @@ def Usage():
     print('skip                         --does\'t regenerate markdown files')
     print('ss                           --both SUMMARY.md and skip')
     exit(0)
+
+class generateCover():
+    coverColor = ({'name':'cover0', 'backgroundColor':(78,9,41), 'fontColor':(123, 31, 231)},
+        {'name':'cover1', 'backgroundColor':(98,111,27), 'fontColor':(188, 125, 70)},
+        {'name':'cover2', 'backgroundColor':(247,74,200), 'fontColor':(55, 249, 158)},
+        {'name':'cover3', 'backgroundColor':(122,118,79), 'fontColor':(17, 53, 12)},
+        {'name':'cover4', 'backgroundColor':(151,55,45), 'fontColor':(85, 228, 60)},
+        {'name':'cover5', 'backgroundColor':(100,0,182), 'fontColor':(43, 249, 170)},
+        {'name':'cover6', 'backgroundColor':(211,112,169), 'fontColor':(114, 21, 74)},
+        {'name':'cover7', 'backgroundColor':(92,187,139), 'fontColor':(51, 30, 248)},
+        {'name':'cover8', 'backgroundColor':(203,138,54), 'fontColor':(140, 201, 38)},
+        {'name':'cover9', 'backgroundColor':(123,137,189), 'fontColor':(177, 183, 162)},
+        {'name':'cover10', 'backgroundColor':(238,220,102), 'fontColor':(146, 7, 239)},
+        {'name':'cover11', 'backgroundColor':(102,12,59), 'fontColor':(250, 30, 159)},
+        {'name':'cover12', 'backgroundColor':(57,12,22), 'fontColor':(49, 172, 27)},
+        {'name':'cover13', 'backgroundColor':(97,102,122), 'fontColor':(163, 159, 21)},
+        {'name':'cover14', 'backgroundColor':(8,99,32), 'fontColor':(208, 144, 192)},
+        {'name':'cover15', 'backgroundColor':(73,220,192), 'fontColor':(249, 222, 120)},
+        )
+    coverDir = 'mycovers'
+    deWidth = 758
+    deHeight = 1024
+    deFontSize = 48
+    deTitleName = '我人有的和主产不为这工要在地一上是asdfvgbhnjmk要在地一上是gbhnjmk要在地一上是bhnjmk'
+    def __init__(self):
+        self.deTitleName = self.deTitleName.upper()
+        if not os.path.exists(self.coverDir):
+            os.mkdir(self.coverDir)
+            for c in self.coverColor:
+                self.generateCover(self.deTitleName, self.deWidth, self.deHeight, self.deFontSize, c)
+        else:
+            self.generateCover(self.deTitleName, self.deWidth, self.deHeight, self.deFontSize, self.coverColor[4])
+
+
+    def generateCover(self, titleName, width, height, fontSize, coverName=None):
+        if isinstance(coverName, dict):
+            cover = coverName
+            jpgName = os.path.join(self.coverDir, cover['name']+'.jpg')
+        else:
+            jpgName = 'cover.jpg'
+            if coverName is None:
+                # use cover according to titleName
+                coverIndex = int(hashlib.sha256(titleName.encode('utf-8')).hexdigest(), 16)%len(self.coverColor)
+                print('use %s'%(self.coverColor[coverIndex]['name']))
+                cover = self.coverColor[coverIndex]
+            else:
+                for c in self.coverColor:
+                    if c['name'] == coverName:
+                        cover = c
+                        break
+                else:
+                    print('%s not found!'%(os.path.join(self.coverDir, coverName)))
+                    exit(0)
+
+        bgcolor = cover['backgroundColor']
+        #print('bgcolor : ', bgcolor)
+        #创建画面对象
+        im = Image.new('RGB', (width, height), bgcolor)
+        #创建画笔对象
+        draw = ImageDraw.Draw(im)
+        #调用画笔的point()函数绘制噪点
+        for i in range(0, 200):
+            x = random.randrange(0, width)
+            y = random.randrange(0, height)
+            fill = (random.randrange(0, 255), 255, random.randrange(0, 255))
+            draw.point((x, y), fill=fill)
+            draw.point((x-1, y-1), fill=fill)
+            draw.point((x+1, y-1), fill=fill)
+            draw.point((x-1, y+1), fill=fill)
+            draw.point((x+1, y+1), fill=fill)
+
+        #构造字体对象，ubuntu的字体路径为“/usr/share/fonts/truetype/freefont”
+        import platform
+        if platform.system() == 'Windows':
+            font = ImageFont.truetype('static/STXIHEI.TTF', fontSize)
+        elif platform.system() == 'Darwin':
+            font = ImageFont.truetype('static/Songti.ttc', fontSize)
+        else:
+            print('have no %s font file '%platform.system())
+
+        #font = ImageFont.load_default().font
+        #构造字体颜色
+        titleLen = fontSize*len(titleName)
+        tmpWidth = width
+        spaceCnt = 2
+        while True:
+            titleStartX = (tmpWidth - titleLen)//spaceCnt
+            if titleStartX >= fontSize:
+                break
+            else:
+                tmpWidth += width
+                spaceCnt += 2
+
+        titleStartY = random.randint(fontSize, height//2)
+        lineLen = len(titleName)//(tmpWidth//width)
+        lineLenPixel = lineLen*fontSize
+
+        # import textwrap
+        # lines = textwrap.wrap(titleName, width=lineLen)
+        # for line in lines:
+        #     print('line : ', line)
+        # exit(0)
+
+        fontcolor = cover['fontColor']
+        # print('titleStartX : %d, titleStartY : %d'%(titleStartX, titleStartY))
+        pos = 0
+        for i in range((tmpWidth//width) + 1):
+            if pos >= len(titleName):
+                break
+            text = titleName[pos:pos+lineLen]
+            for j in range(0, fontSize):
+                realLineLenPixel = draw.textsize(text, font=font)[0]
+                if realLineLenPixel < lineLenPixel:
+                    text = titleName[pos:pos+lineLen+j]
+                elif realLineLenPixel > lineLenPixel:
+                    text = titleName[pos:pos+lineLen+j-2]
+                    pos = pos+lineLen+j-2
+                    break
+                else:
+                    text = titleName[pos:pos+lineLen+j-1]
+                    pos = pos+lineLen+j-1
+                    break
+                if pos+lineLen+j >= len(titleName):
+                    pos = pos+lineLen+j
+                    break
+
+            if i == 0:
+                lineLenPixel = draw.textsize(text, font=font)[0]
+            print('text : ', text)
+            print('size : ', draw.textsize(text, font=font)[0])
+            print('pos : ', pos, 'len : ', len(titleName))
+            print('lineLenPixel : ', lineLenPixel)
+            draw.text((titleStartX, titleStartY+(i*fontSize)), text, font=font, fill=fontcolor, spacing=0, align='left')
+        #释放画笔
+        del draw
+        # im.save(jpgName, 'jpg')
+        im.save(jpgName, 'png')
 
 class Getch:
     """Gets a single character from standard input.  Does not echo to the
@@ -108,6 +248,9 @@ class Html2Book():
     bookFormatList = ('mobi', 'pdfk', 'pdfpc')
     indexMdBak = 'index.md.bak'
     DEBUG = False
+    kindleWidth = 758
+    kindleHeight = 1024
+    titleFontSize = 48
 
     """docstring for Html2Book"""
     def __init__(self):
@@ -358,6 +501,8 @@ if __name__ == '__main__':
         print('This project only support python3!')
         exit(0)
 
+    cgc = generateCover()
+    exit(0)
     chtml2book = Html2Book()
     chtml2book.generateBook()
 
